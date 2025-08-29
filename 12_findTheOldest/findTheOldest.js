2760925c741c8a63d6f0eca97c0bef8e5c79a59d
Zac Shaffer <shafferzac99@gmail.com>
2025-08-29 13:20:34 -0500

    passed findOldest test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestPerson = {};
+    for(let person of arr) {
+        if(getAge(person) > getAge(oldestPerson) || !getAge(oldestPerson)) {
+            oldestPerson = person;
+        }
 
+        console.log(oldestPerson)
+        console.log(getAge(oldestPerson) + " " + getAge(person))
+    }
+
+    return oldestPerson;
 };
+
+function getAge(person) {
+    let birth = person.yearOfBirth;
+    let death = person.yearOfDeath || new Date().getFullYear();
+
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
