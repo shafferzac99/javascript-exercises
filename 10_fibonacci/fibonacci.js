d84fdb083b5ee27236d4f49d6bdd07fffb9e33d9
Zac Shaffer <shafferzac99@gmail.com>
2025-08-29 12:53:37 -0500

    passed fibonacci test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+    try {
+        fibIndex = parseInt(fibIndex);
+    }
+    catch (error){
+        return "OOPS"
+    }
+
+    if(fibIndex < 0 ) return "OOPS"
+
+    let fib = [0,1];
+
+    for (i = 2; i <= fibIndex; i++)
+    {
+        fib.push(fib[i-1] + fib[i-2])
+        console.log(fib)
+        console.log(i)
+    }
+
+    return fib[fibIndex]
 
 };
+
+function parseIndex() {
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
