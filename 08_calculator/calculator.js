c810b0d8654d1b205b12ec79f81d0cc28b280a65
Zac Shaffer <shafferzac99@gmail.com>
2025-08-29 11:56:00 -0500

    passed calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  let sum = 0;
+  for (let num of arr){
+    sum += num;
+  }
+
+  return sum
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  let product = 1;
+  for (let num of arr){
+    product *= num;
+  }
+
+  return product
 };
 
-const sum = function() {
-	
+const power = function(num, exponent) {
+  return Math.pow(num, exponent);
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
+const factorial = function(num) {
+  let factorial = 1
+  for(i=num; i > 0; i--)
+  {
+    factorial *=i;
+  }
 	
-};
-
-const factorial = function() {
-	
+  return factorial;
 };
 
 // Do not edit below this line
