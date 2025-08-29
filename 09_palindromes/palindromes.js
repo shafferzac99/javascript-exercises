61ad90e75b09046a11cb03d28972602d6fdf4a1b
Zac Shaffer <shafferzac99@gmail.com>
2025-08-29 12:33:34 -0500

    passed palindrome test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let wordArray = word.split("").filter(c => c.match(/[a-z0-9]/));
+    let reversedWordArray = wordArray.toReversed();
 
+    console.log(wordArray);
+    console.log(reversedWordArray);
+
+    return wordArray.join('') == reversedWordArray.join('');
 };
 
 // Do not edit below this line
